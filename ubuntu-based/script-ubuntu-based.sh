3277d9bc47e8def80f2c0df7c28fb2dc4bb67699
Plínio Larrubia <plinio2xd@gmail.com>
2020-11-17 11:54:51 -0300

    only install the 2 latest openjdk

--- ubuntu-based/script-ubuntu-based.sh
+++ ubuntu-based/script-ubuntu-based.sh
@@ -231,8 +231,8 @@ sudo $pkg $f_install python3-pip
 sudo $pkg $f_install python-pip
 
 clear
-installCounter "OpenJDK 8, 11 and 14"
-sudo $pkg $f_install openjdk-8-jdk openjdk-11-jdk openjdk-14-jdk
+installCounter "OpenJDK 11 and 14"
+sudo $pkg $f_install openjdk-11-jdk openjdk-14-jdk
 #
 #
 #
