4a814f76301e541804666da6c9ced0d26bb8d8e3
LeDragoX <plinio2xd@gmail.com>
2021-07-12 15:08:44 -0300

    Add WSL2 script

--- README.md
+++ README.md
@@ -13,21 +13,23 @@
 ## <img width=4% src=./images/Ubuntu-icon.webp> For Ubuntu based users, made in Linux Mint.
 
 ```s
-# Dependencies (Ubuntu)
+# Usage - Ubuntu and WSL2
 sudo apt install -fy git
 
+mkdir ~/Downloads
 git clone https://github.com/LeDragoX/LinuxScript.git ~/Downloads/LinuxScript
-cd ~/Downloads/LinuxScript
+cd ~/Downloads/LinuxScript/src
 chmod +x *.sh && yes | ./UbuntuScript.sh
 ```
 
 ## <img width=4% src=./images/ArchLinux-icon.png> For Arch based users, made in Manjaro.
 ```s
-# Dependencies (Arch)
+# Usage - Arch
 sudo pacman -Sy --noconfirm git
 
+mkdir ~/Downloads
 git clone https://github.com/LeDragoX/LinuxScript.git ~/Downloads/LinuxScript
-cd ~/Downloads/LinuxScript
+cd ~/Downloads/LinuxScript/src
 chmod +x *.sh && yes | ./ArchScript.sh
 ```
 
@@ -56,24 +58,4 @@ chmod +x *.sh && yes | ./ArchScript.sh
 
 ## License
 
-    MIT License
-
-    Copyright (c) 2021 Plínio Larrubia
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+  Check the License file [here](LICENSE).
---
+++ src/WSL2UbuntuScript.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+function initVariables {
+
+    # Initialize Global variables
+
+    clear
+    app_num=0
+    total_apps=10
+
+    default_browser="microsoft-edge"
+    config_folder="PKGSConfig"
+    script_folder=$(pwd)
+    wait_time=7
+
+    echo "
+    app_num         = $app_num
+    total_apps      = $total_apps
+
+    default_browser = $default_browser
+    config_folder   = $config_folder
+    script_folder   = $script_folder
+    wait_time       = $wait_time
+    "
+
+    echo ""
+    read -t $wait_time -p "Waiting $wait_time seconds only ..."
+    echo ""
+
+}
+
+function superEcho {
+    echo ""
+    echo "<==================== $1 ====================>"
+    echo ""
+}
+
+function installCounter {
+    superEcho "( $((app_num+=1))/$total_apps ) Installing: [$1]"
+}
+
+function setUpEnv {
+
+    clear
+    echo "To allow comments using on Zsh terminal | Temporary Workaround"
+    setopt interactivecomments
+
+    # 1 - Preparing the files location
+
+    mkdir ~/$config_folder
+    cd ~/$config_folder
+    
+    # Making folders for Custom Themes
+    mkdir ~/.icons
+
+    timedatectl set-local-rtc 1 # Using Local time (Dualboot with Windows)
+    #sudo timedatectl set-timezone UTC # Using UTC
+
+    # 2 - Fix currently installed Packages
+
+    printf "[Adapted] Ubuntu fix broken packages (best solution)\n"
+    sudo apt update -y --fix-missing
+    sudo dpkg --configure -a            # Attempts to fix problems with broken dependencies between program packages.
+    sudo apt-get --fix-broken install
+
+}
+
+function setUpGit {
+    # 3 - Set Up Git Commits Signature (Verified)
+
+    # Install Git first
+    sudo apt install -fy git
+    
+    pushd ~/.gnupg
+        # Import GPG keys
+        gpg --import *.asc
+        # Get the exact key ID from the system
+        # Code adapted from: https://stackoverflow.com/a/66242583        # My key name
+        keyID=$(gpg --list-signatures --with-colons | grep 'sig' | grep 'plinio' | head -n 1 | cut -d':' -f5)
+        git config --global user.signingkey $keyID
+        # Always commit with GPG signature
+        git config --global commit.gpgsign true
+    popd
+}
+
+function installPackages {
+
+    # 4 - Install Apt Packages
+
+    sudo dpkg --add-architecture i386                                               # Enable 32-bits Architecture
+    sudo DEBIAN_FRONTEND=noninteractive apt install -fy ubuntu-restricted-extras    # Remove interactivity | Useful proprietary stuff
+
+    declare -a apt_pkgs=(
+
+        # Initial Libs that i use
+
+        "adb"           # Android Debugging
+        "curl"          # Terminal Download Manager
+        "fastboot"      # Android Debugging
+        "gdebi"         # CLI/GUI .deb Installer
+        "gdebi-core"    # CLI/GUI .deb Installer
+        "htop"          # Terminal System Monitor
+        "neofetch"      # Neofetch Command
+        "vim"           # Terminal Text Editor
+        "wget"          # Terminal Download Manager
+
+        # Programming languages for devlopment
+
+        "nodejs"        # NodeJS
+    )
+
+    printf "\nInstalling via Advanced Package Tool (apt)...\n"
+    for App in ${apt_pkgs[@]}; do
+        printf "\nInstalling: $App \n"
+        sudo apt install -fy $App
+    done
+
+    # Yarn for NodeJS
+    npm install --global yarn
+
+}
+
+function installZsh {
+
+    # 5 - Install Zsh
+
+    printf "Zsh install\n"
+    sudo apt install -fy zsh
+    printf "Make Zsh the default shell\n"
+    chsh -s $(which zsh)
+    $SHELL --version
+
+    printf "Needs to log out and log in to make the changes\n"
+    echo $SHELL
+
+    printf "Oh My Zsh\n"
+    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+}
+
+function updateAll {
+
+    # 6 - Update System
+
+    sudo apt update -y
+    sudo apt dist-upgrade -fy
+    sudo apt autoclean -y       # limpa seu repositório local de todos os pacotes que o APT baixou.
+    sudo apt autoremove -y      # remove dependências que não são mais necessárias ao seu Sistema.
+
+}
+
+initVariables
+setUpEnv
+
+setUpGit
+installPackages
+installZsh
+updateAll
