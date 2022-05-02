2db2ceea0026e628fb2a2b0b48f47d86ed5c88c0
Plínio Larrubia <plinio2xd@gmail.com>
2022-05-02 13:34:41 -0300

    feat: Improve Linux script menu

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -2,8 +2,115 @@
 
 source ./src/lib/base-script.sh
 
-function main() {
+function main_menu() {
+  PS3="Select an option to see the scripts: "
+  select option in "Exit" "Arch Scripts" "Ubuntu Scripts" "Utilities Scripts"; do
+    echo "You choose $option"
+    case $option in
+    "Exit")
+      echo "Exiting..." && echo
+      break
+      ;;
+    "Arch Scripts")
+      echo "- Arch" && echo
+      arch_menu
+      ;;
+    "Ubuntu Scripts")
+      echo "- Ubuntu" && echo
+      ubuntu_menu
+      ;;
+    "Utilities Scripts")
+      echo "- Utilities" && echo
+      utilities_menu
+      ;;
+    *)
+      echoError "ERROR: Invalid Option"
+      main
+      ;;
+    esac
+    break
+  done
+}
 
+function arch_menu() {
+  clear
+  PS3="Select the script: "
+  select script in "Go Back" "arch-script.sh" "wsl-arch-pre-setup.sh" "wsl-arch-script.sh"; do
+    echo "You chose the $script"
+    case $script in
+    "Go Back")
+      main
+      ;;
+    "arch-script.sh")
+      echo "- I use Arch btw..." && echo
+      ./src/scripts/$script
+      ;;
+    "wsl-arch-pre-setup.sh")
+      echo "- Arch WSL Pre Setup" && echo
+      ./src/scripts/$script
+      ;;
+    "wsl-arch-script.sh")
+      echo "- Arch WSL" && echo
+      ./src/scripts/$script
+      ;;
+    *)
+      echoError "ERROR: Invalid Option"
+      arch_menu
+      ;;
+    esac
+    break
+  done
+}
+
+function ubuntu_menu() {
+  clear
+  PS3="Select the script: "
+  select script in "Go Back" ubuntu-script.sh wsl-ubuntu-script.sh; do
+    echo "You chose the $script"
+    case $script in
+    "Go Back")
+      main
+      ;;
+    ubuntu-script.sh)
+      echo "- Ubuntu" && echo
+      ./src/scripts/$script
+      ;;
+    wsl-ubuntu-script.sh)
+      echo "- Ubuntu WSL" && echo
+      ./src/scripts/$script
+      ;;
+    *)
+      echoError "ERROR: Invalid Option"
+      ubuntu_menu
+      ;;
+    esac
+    break
+  done
+}
+
+function utilities_menu() {
+  clear
+  PS3="Select the script: "
+  select script in "Go Back" git-gpg-ssh-setup.sh; do
+    echo "You chose the $script"
+    case $script in
+    "Go Back")
+      main
+      ;;
+    git-gpg-ssh-setup.sh)
+      echo "- Git, GPG and SSH setup (UTILS)" && echo
+      ./src/utils/$script
+      ;;
+    *)
+      echoError "ERROR: Invalid Option"
+      utilities_menu
+      ;;
+    esac
+    break
+  done
+}
+
+function main() {
   clear
   echo "<=====================================================================================>"
   echo '888      d8b                             .d8888b.                   d8b          888   '
@@ -21,40 +128,7 @@ function main() {
   echoError '                                    Made by LeDragoX'
   echo "<=====================================================================================>"
 
-  PS3="Select the script: "
-  select Script in arch-script.sh ubuntu-script.sh wsl-ubuntu-script.sh wsl-arch-pre-setup.sh wsl-arch-script.sh git-gpg-ssh-setup.sh; do
-    echo "You chose the $Script"
-    case $Script in arch-script.sh)
-      echo "- I use Arch btw..." && echo
-      ./src/scripts/$Script
-      ;;
-    ubuntu-script.sh)
-      echo "- Ubuntu" && echo
-      ./src/scripts/$Script
-      ;;
-    wsl-ubuntu-script.sh)
-      echo "- Ubuntu WSL" && echo
-      ./src/scripts/$Script
-      ;;
-    wsl-arch-pre-setup.sh)
-      echo "- Arch WSL Pre Setup" && echo
-      ./src/scripts/$Script
-      ;;
-    wsl-arch-script.sh)
-      echo "- Arch WSL" && echo
-      ./src/scripts/$Script
-      ;;
-    git-gpg-ssh-setup.sh)
-      echo "- Git, GPG and SSH setup (UTILS)" && echo
-      ./src/utils/$Script
-      ;;
-    *)
-      echo "ERROR: Invalid Option"
-      ;;
-    esac
-    break
-  done
-
+  main_menu
   echo "EXIT CODE: $?"
 }
 
