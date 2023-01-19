38d9bf1825ade31ea2001b89dba7a989cc175d49
Plínio Larrubia <plinio2xd@gmail.com>
2023-01-19 18:41:14 -0300

    Add .gpg files to .gitignore

--- src/scripts/ubuntu-script.sh
+++ src/scripts/ubuntu-script.sh
@@ -44,7 +44,7 @@ function installPpaKeysUbuntu() {
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
   sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
   sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
+  sudo rm packages.microsoft.gpg
 }
 
 function installPackagesUbuntu() {
