90049277e1460c14c47d89c0b41d688e93d00c5f
Plínio Larrubia <plinio2xd@gmail.com>
2023-01-19 18:11:17 -0300

    Attempt to fix node/yarn install - Also insert a param to rvm install

--- src/lib/base-script.sh
+++ src/lib/base-script.sh
@@ -38,11 +38,13 @@ function installProgrammingLanguagesWithVersionManagers() {
   echoCaption "Installing latest LTS Node..."
   nvm install --lts
   node -v
+  corepack enable
+  yarn -v
 
   echoSection "RVM - Ruby Version Manager (With Rails)"
   gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  \curl -sSL https://get.rvm.io | bash -s stable --rails
+  \curl -sSL https://get.rvm.io | bash -s stable --rails --auto-dotfiles
   rvm -v
   rvm requirements run
   rvm install ruby
